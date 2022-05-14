04bf8c2155d33c7303a33488a4851a375ccb7be5
Kaan Arslan <oguzkaanarslan@outlook.com>
2022-05-14 15:48:54 +0300

    navbar and auth page first commit

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Signin from "./pages/Auth/Signin";
+import Signup from "./pages/Auth/Signup";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+
+        <div id="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+export default App;
