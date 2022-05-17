ad1a66bf91285ab26d87dfeabd704ccff6c3ebec
Kaan Arslan <oguzkaanarslan@outlook.com>
2022-05-17 19:02:55 +0300

    add login page

--- client/src/pages/Auth/Signin/index.js
+++ client/src/pages/Auth/Signin/index.js
@@ -1,9 +1,100 @@
-import React from 'react'
+import React from "react";
+import { useAuth } from "../../../contexts/AuthContext";
+import {
+  Flex,
+  Box,
+  Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Alert,
+} from "@chakra-ui/react";
+import { useFormik } from "formik";
+import validationSchema from "./validations";
+import { fetchLogin } from "../../../api";
+import {  useNavigate } from "react-router-dom";
 
 function Signin() {
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      passwordConfirm: "",
+    },
+    validationSchema,
+
+    onSubmit: async (values, bag) => {
+      try {
+        const loginResponse = await fetchLogin({
+          email: values.email,
+          password: values.password,
+        });
+        login(loginResponse);
+        navigate("/profile")
+        console.log(loginResponse);
+      } catch (err) {
+        bag.setErrors({ general: err.response.data.message });
+      }
+    },
+  });
   return (
-    <div>Signin</div>
-  )
+    <div>
+      <Flex align="center" width="full" justifyContent="center">
+        <Box pt="10">
+          <Box textAlign="center">
+            <Heading>Sign In</Heading>
+          </Box>
+          <Box my="5">
+            {formik.errors.general && (
+              <Alert status="error">{formik.errors.general}</Alert>
+            )}
+          </Box>
+          <Box my="5" textAlign="left">
+            <form onSubmit={formik.handleSubmit}>
+              <FormControl>
+                <FormLabel>E-mail</FormLabel>
+                <Input
+                  name="email"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
+                  isInvalid={formik.touched.email && formik.errors.email}
+                />
+              </FormControl>
+
+              <FormControl mt="4">
+                <FormLabel>Password</FormLabel>
+                <Input
+                  name="password"
+                  type="password"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.password}
+                  isInvalid={formik.touched.password && formik.errors.password}
+                />
+              </FormControl>
+
+              
+
+              <Button
+                mt="4"
+                width="full"
+                type="submit"
+                backgroundColor="blue"
+                color="white"
+              >
+                Sign In
+              </Button>
+            </form>
+          </Box>
+        </Box>
+      </Flex>
+    </div>
+  );
 }
 
-export default Signin
+export default Signin;
