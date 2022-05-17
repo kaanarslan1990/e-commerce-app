ab622db540ae56e830980135f834af0f86bd3645
Kaan Arslan <oguzkaanarslan@outlook.com>
2022-05-17 17:17:28 +0300

    logout page added

--- client/src/api.js
+++ client/src/api.js
@@ -1,22 +1,24 @@
 import axios from "axios";
 
 // Add a request interceptor
-axios.interceptors.request.use(function (config) {
-    const {origin} = new URL(config.url)
+axios.interceptors.request.use(
+  function (config) {
+    const { origin } = new URL(config.url);
 
     const allowedOrigins = [process.env.REACT_APP_BASE_ENDPOINT];
-    const token = localStorage.getItem('access-token');
+    const token = localStorage.getItem("access-token");
 
     if (allowedOrigins.includes(origin)) {
-        config.headers.authorization = token
+      config.headers.authorization = token;
     }
     // Do something before request is sent
     return config;
-  }, function (error) {
+  },
+  function (error) {
     // Do something with request error
     return Promise.reject(error);
-  });
-
+  }
+);
 
 export const fetchProductList = async ({ pageParam = 0 }) => {
   const { data } = await axios.get(
@@ -46,6 +48,17 @@ export const fetchRegister = async (input) => {
 export const fetchMe = async () => {
   const { data } = await axios.get(
     `${process.env.REACT_APP_BASE_ENDPOINT}/auth/me`
+  );
+
+  return data;
+};
+
+export const fetchLogout = async () => {
+  const { data } = await axios.post(
+    `${process.env.REACT_APP_BASE_ENDPOINT}/auth/logout`,
+    {
+      refresh_token: localStorage.getItem("refresh-token"),
+    }
   );
 
   return data;
--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,17 +1,33 @@
-import { Text } from '@chakra-ui/react'
-import React from 'react'
-import { useAuth } from '../../contexts/AuthContext'
+import { Button, Text } from "@chakra-ui/react";
+import React from "react";
+import { useAuth } from "../../contexts/AuthContext";
+import {useNavigate} from 'react-router-dom';
 
 function Profile() {
-    const {user} =useAuth()
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    logout(() => {  
+      navigate('/')
+    });
+  };
   return (
     <div>
-    <Text fontSize="24" as="b" color="purple">Profile</Text>
-    <br />
-    <br />
-        <code>{JSON.stringify(user)}</code>
+      <Text fontSize="24" as="b" color="purple">
+        Profile
+      </Text>
+      <br />
+      <br />
+      <code>{JSON.stringify(user)}</code>
+      <br />
+      <br />
+      <br />
+      <br />
+      <Button colorScheme="purple" variant="solid" onClick={handleLogout}>
+        Logout
+      </Button>
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
