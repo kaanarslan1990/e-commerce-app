13c10fdf8d099521bf032b5b9073abd68b5404f1
Kaan Arslan <oguzkaanarslan@outlook.com>
2022-05-19 00:13:44 +0300

    admin routing page added

--- client/src/App.js
+++ client/src/App.js
@@ -33,7 +33,7 @@ function App() {
               }
             />
             <Route
-              path="/admin"             
+              path="/admin/*"             
               element={
                 <ProtectedRoute  admin={true}>
                   <Admin />
--- client/src/pages/Admin/index.js
+++ client/src/pages/Admin/index.js
@@ -1,16 +1,36 @@
+import { Box } from "@chakra-ui/react";
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Route, Routes } from "react-router-dom";
+import Products from "./Products";
+import Home from "./Home";
+import Orders from "./Orders";
+import "./style.css";
 
 function Admin() {
+    // const {path, url} = useMatch();
   return (
     <div>
       <nav>
         <ul className="admin-menu">
           <li>
-            <NavLink to="/">AdminHome</NavLink>
+            <NavLink to="/admin">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/admin/orders">Orders</NavLink>
+          </li>
+          <li>
+            <NavLink to="/admin/products">Product</NavLink>
           </li>
         </ul>
       </nav>
+
+      <Box mt="10">
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/orders" element={<Orders />}/>
+          <Route path="/products" element={<Products />}/>
+        </Routes>
+      </Box>
     </div>
   );
 }
